5583a2e355571b2443c8692df9851d9eaa832f92
Joe Pell <jpell3130@gmail.com>
2019-02-26 14:56:18 -0500

    04a_countries_bonus

--- lib/04a_countries_bonus.rb
+++ lib/04a_countries_bonus.rb
@@ -17,6 +17,11 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  SELECT name FROM countries
+  WHERE gdp > ALL(
+    SELECT COALESCE(gdp, 0) FROM countries
+    WHERE continent = 'Europe'
+  ) 
   SQL
 end
 
@@ -24,6 +29,10 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+  SELECT continent, name, area FROM countries
+  WHERE area IN(
+    SELECT MAX(area) FROM countries GROUP BY continent
+  )
   SQL
 end
 
@@ -31,5 +40,10 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT a.name, a.continent FROM countries a
+  WHERE a.population / 3 > ALL(
+    SELECT b.population FROM countries b
+    WHERE a.continent = b.continent AND a.name != b.name
+  )
   SQL
 end
